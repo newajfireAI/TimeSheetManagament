a3a88f3446aad341556eb445952cf8256612b81d
newajfireAI <fireinfo.newaj@gmail.com>
2025-10-20 16:38:12 +0600

    navBar design created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,32 @@
+"use client"
+import Image from 'next/image'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 
 function Navbar() {
+  const pathName = usePathname()
+  console.log(pathName);
 
   return (
-    <div className='bg-white w-screen h-[80px] text-black py-6 px-12 gap-12 flex justify-center items-center'>
-        <div>1</div>
-        <div className='flex-1'>2</div>
-        <div>3</div>
+    <div className='bg-white w-full h-[80px] text-black py-6 px-12 gap-12 flex justify-center items-center'>
+        <div className='w-[48px] h-[48px]'>
+          <Image className='w-full h-full scale-150' src={'/assets/logo.png'} width={100} height={100} />
+        </div>
+        <div className='flex-1'>
+          <h3><span className='font-semibold text-3xl'>Overview</span>{pathName}</h3>
+        </div>
+        <div>
+          <div className='flex items-center gap-3'>
+            <div className='text-right'>
+              <h3 className='text-[20px] font-bold'>Steve john</h3>
+              <p className='text-[#736E6E]'>example@gmail.com</p>
+            </div>
+            <div className='w-[48px] h-[48px]'>
+              <Image className='w-full h-full rounded-full' src={'/assets/profilePlaceholder.png'} width={80} height={80}/>
+            </div>
+
+          </div>
+        </div>
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,8 +4,8 @@ import React, { useState } from 'react'
 function Sidebar() {
     const [fullview, setFullView] = useState(true) // TODO: we have to make it false
   return (
-    <div onClick={()=> setFullView(!fullview)} className={`${fullview ? 'bg-orange-300 w-[80px]': 'bg-green-500 w-[160px]'} h-screen`}>
-        
+    <div onClick={()=> setFullView(!fullview)} className={`${fullview ? 'bg-orange-300 w-[5%]': 'bg-green-500 w-[15%]'} h-scrin px-4 py-6 fixed`}>
+        sdfsdfsdf
     </div>
   )
 }
