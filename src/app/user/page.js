2150137606f88e3108233e024e93a48d63bbb43d
neaz morshed sajid <neazmorshed407@gmail.com>
2025-10-21 17:54:41 +0600

    user running

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -1,14 +1,28 @@
+"use client";
 import Container from "@/components/container";
-import React from "react";
+import React, { useState } from "react";
 import { MdModeEdit } from "react-icons/md";
-import { FaCaretDown } from "react-icons/fa";
+import { FaCaretDown, FaCaretUp, FaRegEyeSlash } from "react-icons/fa";
+import { IoEyeOutline } from "react-icons/io5";
 
 const page = () => {
+  const [showPass, setShowPass] = useState(false);
+  const [showNewPass, setShowNewPass] = useState(false);
+  const [Role,setRole] = useState("User");
+   const [showRole,setShowRole] = useState(false);
+   const [Gender,setGender] = useState("Male");
+   const [showGender,setShowGender] = useState(false);
+
+  const roles= ["User","Vender","Employee" ];
+  const gender= ["Male","Female","Others" ];
+
+
+
   return (
     <div >
       <form className="py-8">
         <div className="flex flex-col gap-2 ">
-          <label className="font-roboto text-[16px]">
+          <label className="font-roboto text-[#000000] text-[16px]">
             Name <span className="text-[#F46B6A]">*</span>
           </label>
           <input
@@ -18,7 +32,7 @@ const page = () => {
         </div>
 
         <div className="flex flex-col gap-2 mt-4 ">
-          <label className="font-roboto text-[16px]">
+          <label className="font-roboto text-[#000000] text-[16px]">
             Email <span className="text-[#F46B6A]">*</span>
           </label>
           <input
@@ -29,7 +43,7 @@ const page = () => {
 
         <div className="flex justify-between gap-[2%] mt-4 ">
           <div className="flex flex-col gap-2 mt-4 w-full  ">
-            <label className="font-roboto text-[16px]">
+            <label className="font-roboto text-[#000000] text-[16px]">
               Phone <span className="text-[#F46B6A]">*</span>
             </label>
             <div className="relative">
@@ -44,66 +58,108 @@ const page = () => {
           </div>
 
           <div className="flex flex-col gap-2 mt-4  w-full">
-            <label className="font-roboto text-[16px]">
+            <label className="font-roboto text-[#000000] text-[16px]">
               Gender<span className="text-[#F46B6A]">*</span>
             </label>
-            <div className="relative">
+            <div className="relative"
+            onClick={() => setShowGender(!showGender)}
+            >
               <input
+              readOnly
                 className="w-full bg-white outline-none py-[18px] px-4 text-[#6D6E73] font-roboto text-[16px] placeholder-[#6D6E73] border border-[#CED2E5] rounded "
-                placeholder="Phone Number"
+                placeholder={Gender}
               />
               <div className="w-6 h-6 bg-[#F2F4FF] flex items-center justify-center absolute top-1/2 -translate-y-1/2 right-4 text-[#6D6E73]">
-                <FaCaretDown />
+                {showGender? <FaCaretUp />:<FaCaretDown />}
+            </div>
+            <div className={`w-full text-center bg-white font-inter text-[14px] text-[#333333] z-30 absolute ${showGender ? "opacity-100 h-auto visible overflow-auto" : "opacity-0 h-0 invisible overflow-hidden"}`}>
+                  {gender.map((item) => (
+                    <div key={item} onClick={() => { setGender(item); setShowGender(false); }} className="py-2 hover:bg-[#5069E5] hover:text-white cursor-pointer">{item}</div>
+                  ))}
               </div>
             </div>
           </div>
         </div>
 
-        <div className="flex flex-col gap-2 mt-4">
-          <label className="font-roboto text-[16px]">
+        
+
+        <div className="flex justify-between gap-[2%] mt-4 ">
+
+        <div className="flex flex-col gap-2 mt-4 w-full">
+          <label className="font-roboto text-[#000000] text-[16px]">
             Role<span className="text-[#F46B6A]">*</span>
           </label>
-          <div className="relative">
+          <div className="relative"
+          onClick={() => setShowRole(!showRole)}
+          >
+             <div >
+            
             <input
+              readOnly
               className="w-full bg-white outline-none py-[18px] px-4 text-[#6D6E73] font-roboto text-[16px] placeholder-[#6D6E73] border border-[#CED2E5] rounded "
-              placeholder="Phone Number"
+              placeholder={Role}
             />
-            <div className="w-6 h-6 bg-[#F2F4FF] flex items-center justify-center absolute top-1/2 -translate-y-1/2 right-4 text-[#6D6E73]">
-              <FaCaretDown />
+            <div 
+            
+            className="w-6 h-6 bg-[#F2F4FF] flex items-center justify-center absolute top-1/2 -translate-y-1/2 right-4 text-[#6D6E73]">
+              
+              {showRole? <FaCaretUp />:<FaCaretDown />}
+            </div>
+            <div className={`w-full text-center bg-white font-inter text-[14px] text-[#333333] z-30 absolute ${showRole ? "opacity-100 h-auto visible overflow-auto" : "opacity-0 h-0 invisible overflow-hidden"}`}>
+                  {roles.map((item) => (
+                    <div key={item} onClick={() => { setRole(item); setShowRole(false); }} className="py-2 hover:bg-[#5069E5] hover:text-white cursor-pointer">{item}</div>
+                  ))}
+                </div>
             </div>
           </div>
         </div>
 
-        <div className="flex justify-between gap-[2%] mt-4 ">
+
+
+
           <div className="flex flex-col gap-2 mt-4  w-full">
-            <label className="font-roboto text-[16px]">
+            <label className="font-roboto text-[#000000] text-[16px]">
               Password <span className="text-[#F46B6A]">*</span>
             </label>
             <div className="relative">
               <input
-                className="w-full bg-white outline-none py-[13px] px-4 text-[#6D6E73] font-roboto text-[16px] placeholder-[#6D6E73] border border-[#CED2E5] rounded "
-                placeholder="Phone Number"
+               type={showPass ? "text" : "password"}
+                className="w-full bg-white outline-none py-[18px] px-4 text-[#3D3D40] font-roboto text-[16px] placeholder-[#3D3D40] border border-[#CED2E5] rounded "
+                placeholder=""
               />
-              <div className="w-6 h-6 bg-[#F2F4FF] flex items-center justify-center absolute top-1/2 -translate-y-1/2 right-4 text-[#6D6E73]">
-                <FaCaretDown />
+              <div 
+               onClick={() => setShowPass(!showPass)}
+              className="w-6 h-6  flex items-center justify-center absolute top-1/2 -translate-y-1/2 right-4 text-[#6D6E73]">
+                 {showPass ? (
+                  <IoEyeOutline className="text-[#6D6E73] w-6 h-6 " />
+                ) : (
+                  <FaRegEyeSlash className="text-[#6D6E73] w-6 h-6 " />
+                )}
               </div>
             </div>
           </div>
 
-          <div className="flex flex-col gap-2 mt-4  w-full">
-            <label className="font-roboto text-[16px]">
+          {/* <div className="flex flex-col gap-2 mt-4  w-full">
+            <label className="font-roboto text-[#000000] text-[16px]">
               Confirm Password<span className="text-[#F46B6A]">*</span>
             </label>
             <div className="relative">
               <input
-                className="w-full bg-white outline-none py-[13px] px-4 text-[#6D6E73] font-roboto text-[16px] placeholder-[#6D6E73] border border-[#CED2E5] rounded "
-                placeholder="Phone Number"
+               type={showNewPass ? "text" : "password"}
+                className="w-full bg-white outline-none py-[13px] px-4 text-[#3D3D40] font-roboto text-[16px] placeholder-[#3D3D40] border border-[#CED2E5] rounded "
+                placeholder=""
               />
-              <div className="w-6 h-6 bg-[#F2F4FF] flex items-center justify-center absolute top-1/2 -translate-y-1/2 right-4 text-[#6D6E73]">
-                <FaCaretDown />
+              <div 
+               onClick={() => setShowNewPass(!showNewPass)}
+              className="w-6 h-6  flex items-center justify-center absolute top-1/2 -translate-y-1/2 right-4 text-[#6D6E73]">
+                 {showNewPass ? (
+                  <IoEyeOutline className="text-[#6D6E73] w-6 h-6 " />
+                ) : (
+                  <FaRegEyeSlash className="text-[#6D6E73] w-6 h-6 " />
+                )}
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </form>
       <div className="mt-8 w-[15%] flex justify-between mx-auto">
@@ -119,5 +175,6 @@ const page = () => {
     </div>
   );
 };
+
 
 export default page;
